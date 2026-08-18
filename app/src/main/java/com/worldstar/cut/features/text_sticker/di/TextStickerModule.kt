e6fc4e3ba05e1951677981096803f12453caf7d0
god-s-only <joshxchris098@gmail.com>
2026-08-18 22:11:48 +0100

    feat(text_sticker): TextStickerModule DI placeholder

---
+++ app/src/main/java/com/worldstar/cut/features/text_sticker/di/TextStickerModule.kt
@@ -0,0 +1,9 @@
+package com.worldstar.cut.features.text_sticker.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TextStickerModule
